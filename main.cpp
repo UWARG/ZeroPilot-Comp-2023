72ea6e5dc136d690a8cc2dd813b87a7ae44fc0ac
Gordon Fountain <Gordonthecellist@gmail.com>
2023-02-07 10:01:18 -0500

    Re-add SM as a thread

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "main.h"
 
+#include "FreeRTOS.h"
 #include "LOS_Actuators.hpp"
 #include "SM.hpp"
 #include "cmsis_os2.h"
+#include "task.h"
 
 void SMOperationTask(void *pvParameters);
 const static auto SM_PERIOD_MS = 5;
@@ -12,6 +14,18 @@ int main() {
 
     TaskHandle_t SM_handle = NULL;
 
+    xTaskCreate(SMOperationTask, "SM Thread", 400U, NULL, osPriorityNormal, &SM_handle);
+
+    losKernelStart();
+
+    // should not get here bec losInit() starts the scheduler
+    while (1) {
+    }
+
+    return 0;
+}
+
+void SMOperationTask(void *pvParameters) {
     SM::SystemManager SM_instance;
 
     TickType_t xNextWakeTime;
@@ -20,13 +34,4 @@ int main() {
         SM_instance.execute();
         vTaskDelayUntil(&xNextWakeTime, SM_PERIOD_MS);
     }
-
-    // What does LOS need to operate here
-    losKernelStart();
-
-    // should not get here bec losInit() starts the scheduler
-    while (1) {
-    }
-
-    return 0;
 }
